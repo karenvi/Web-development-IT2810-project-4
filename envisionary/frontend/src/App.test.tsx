0a77f3c58a08fde3974e937026a89a10b7d16c02
Quynh-Lan Nguyen Pham <qlpham@stud.ntnu.no>
2022-11-21 19:12:27 +0100

    #22 Added a test for App that checks the toggle for darkmode

--- envisionary/frontend/src/App.test.tsx
+++ envisionary/frontend/src/App.test.tsx
@@ -1,9 +1,9 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 import { RecoilRoot } from 'recoil';
 import { MockedProvider } from "@apollo/client/testing";
 import { mocks } from './__test__/mocks/mocks';
-
+import userEvent from '@testing-library/user-event';
 
 
 it("App renders correctly", () => {
@@ -16,4 +16,23 @@ it("App renders correctly", () => {
     );
     expect(container).toMatchSnapshot();
   });
+
+  it('Tests the toggle from darkmode to lightmode', async () => {
+    // ARRANGE
+    render(
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <RecoilRoot>
+          <App/>
+        </RecoilRoot>
+      </MockedProvider>
+    )
+
+    // ACT
+    userEvent.click(screen.getByRole("checkbox", {name: "Theme switch"}))
+
+    // ASSERT
+    expect(await screen.findByRole("table")).toHaveStyle('backgroundColor: white; color: black')
+
+  })
+
 
